66867cf024f223a1b22a18bda66db1f13fdb1159
Vijay Anand <iavian@Vijays-MacBook-Pro.local>
2020-09-18 11:43:07 +0530

    a

--- Package.swift
+++ Package.swift
@@ -1,18 +1,17 @@
-// swift-tools-version:5.3
-
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-  name: "Website",
-  dependencies: [
-        
-  ],
-  targets: [
-    .target(
-        name: "Website",
-        dependencies: []),
-    .testTarget(
-        name: "WebsiteTests",
-        dependencies: ["Website"]),
-  ]
+    name: "KYDrawerController",
+    platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
+    products: [
+        .library(name: "KYDrawerController", targets: ["KYDrawerController"])
+    ],
+    targets: [
+        .target(
+            name: "KYDrawerController",
+            path: "KYDrawerController/Classes",
+            exclude: ["SwiftUI"]
+        )
+    ]
 )
